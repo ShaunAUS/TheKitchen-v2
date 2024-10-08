76c38cd558d201ed4dc4756a3f11cc459d8f66bd
ShaunAUS <wnsqud70@gmail.com>
2024-10-08 13:42:31 +0900

    #5 - memberInfoDto 수정

--- kotlinPractice/src/main/kotlin/com/example/kotlinPractice/feature/member/api/dto/MemberInfoDto.kt
+++ kotlinPractice/src/main/kotlin/com/example/kotlinPractice/feature/member/api/dto/MemberInfoDto.kt
@@ -1,31 +1,26 @@
 package com.example.kotlinPractice.feature.member.api.dto
 
-import com.example.kotlinPractice.feature.kitchen.api.dto.KitchenInfoDto
 import com.example.kotlinPractice.domain.entity.Member
 import com.example.kotlinPractice.domain.enums.LevelType
 import com.example.kotlinPractice.domain.enums.SectionType
 
-
 data class MemberInfoDto(
-        val name: String,
-        val level: LevelType,
-        val section: SectionType,
-        val experience: Int?,
-        val kitchen: KitchenInfoDto,
+    val name: String,
+    val level: LevelType,
+    val section: SectionType,
+    val experience: Int,
+    val uniqueId: String,
 ) {
 
-    //TODO converter
     companion object {
         fun of(member: Member): MemberInfoDto {
             return MemberInfoDto(
-                    name = member.name,
-                    level = LevelType.intToType(member.level),
-                    section = SectionType.intToType(member.section),
-                    experience = member.experience,
-                    kitchen = KitchenInfoDto.of(member.kitchen)
+                name = member.name,
+                level = LevelType.intToType(member.level),
+                section = SectionType.intToType(member.section),
+                experience = member.experience,
+                uniqueId = member.uniqueId,
             )
         }
-
     }
 }
-
